cf25bff0ed6ff85411370097b57cc20c3c16d7f0
Kgs. Wahyu <kgswahyu80@gmail.com>
2023-03-25 12:05:45 +0700

    adding gitignore

---
+++ assets/js/main.js
@@ -0,0 +1,139 @@
+$(function() {
+    
+    "use strict";
+    
+    //===== Prealoder
+    
+    $(window).on('load', function(event) {
+        $('#preloader').delay(500).fadeOut(500);
+    });
+    
+    
+    //===== Mobile Menu 
+    
+    $(".navbar-toggler").on('click', function() {
+        $(this).toggleClass('active');
+        $(".navbar-collapse").toggleClass("show");
+    });
+    
+    $(".navbar-nav a").on('click', function() {
+        $(".navbar-toggler").removeClass('active');
+    });
+    
+    
+    //===== close navbar-collapse when a  clicked
+    
+    $(".navbar-nav a").on('click', function () {
+        $(".navbar-collapse").removeClass("show");
+    });
+    
+    
+    //===== Sticky
+    
+    $(window).on('scroll',function(event) {    
+        var scroll = $(window).scrollTop();
+        if (scroll < 10) {
+            $(".navigation").removeClass("sticky");
+        }else{
+            $(".navigation").addClass("sticky");
+        }
+    });
+    
+    
+    //===== Section Menu Active
+    
+    var scrollLink = $('.page-scroll');
+        // Active link switching
+        $(window).scroll(function() {
+        var scrollbarLocation = $(this).scrollTop();
+
+        scrollLink.each(function() {
+
+          var sectionOffset = $(this.hash).offset().top - 90;
+
+          if ( sectionOffset <= scrollbarLocation ) {
+            $(this).parent().addClass('active');
+            $(this).parent().siblings().removeClass('active');
+          }
+        });
+    });
+    
+    
+    //====== Magnific Popup
+    
+    $('.video-popup').magnificPopup({
+        type: 'iframe'
+        // other options
+    });
+    
+    
+    //===== Slick
+
+    $('.testimonial-active').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        speed: 800,
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    arrows: false,
+                }
+            }
+        ]
+    });
+    
+    
+    //===== Back to top
+    
+    // Show or hide the sticky footer button
+    $(window).on('scroll', function(event) {
+        if($(this).scrollTop() > 600){
+            $('.back-to-top').fadeIn(200)
+        } else{
+            $('.back-to-top').fadeOut(200)
+        }
+    });
+    
+    
+    //Animate the scroll to yop
+    $('.back-to-top').on('click', function(event) {
+        event.preventDefault();
+        
+        $('html, body').animate({
+            scrollTop: 0,
+        }, 1500);
+    });
+    
+});
+
+function sendEmail() {
+    // mengambil nilai inputan dari pengguna
+    var email = document.getElementById("email").value;
+  
+    // membangun link mailto dengan nilai inputan pengguna
+    var link = "mailto:mpk@smktelkom-jkt.sch.id" +
+               "?subject=" + encodeURIComponent("Tertarik untuk Belajar Lebih Banyak - Newsletter") +
+               "&body=" + encodeURIComponent("Dear Tim Support,\n\nSaya sangat tertarik untuk menerima newsletter dan tetap berhubungan dengan MPK. Berikut adalah informasi saya:\n\nEmail: " + email + "\n\nTerima kasih atas waktu dan perhatiannya.\n\nSalam saya yang terbaik,\n" + email);
+  
+    // membuka aplikasi email default pengguna dengan link mailto
+    window.location.href = link;
+  }
